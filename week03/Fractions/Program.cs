dbd6e63da70f065e051fc8c05c61f515e73d6a2a
BradGregersen <164933411+BradGregersen@users.noreply.github.com>
2025-03-17 15:35:15 -0600

    Fractions program written

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -1,9 +1,25 @@
 using System;
 
-class Program
+public class Program
 {
-    static void Main(string[] args)
+    public static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
+
+        Fraction fraction1 = new Fraction();
+        Console.WriteLine("Fraction 1: " + fraction1.GetFractionString());
+        Console.WriteLine("Decimal Value: " + fraction1.GetDecimalValue());
+
+        Fraction fraction2 = new Fraction(5);
+        Console.WriteLine("Fraction 2: " + fraction2.GetFractionString());
+        Console.WriteLine("Decimal Value: " + fraction2.GetDecimalValue());
+
+        Fraction fraction3 = new Fraction(3, 4);
+        Console.WriteLine("Fraction 3: " + fraction3.GetFractionString());
+        Console.WriteLine("Decimal Value: " + fraction3.GetDecimalValue());
+
+        fraction3.SetTop(7);
+        fraction3.SetBottom(8);
+        Console.WriteLine("Updated Fraction 3: " + fraction3.GetFractionString());
+        Console.WriteLine("Updated Decimal Value: " + fraction3.GetDecimalValue());
     }
 }
